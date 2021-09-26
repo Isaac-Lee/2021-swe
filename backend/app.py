0e1cbfc0564fa614e360b528ea6996e233f8089a
sonyi00 <ksl2950@naver.com>
2021-09-26 16:49:07 +0900

    create login/join api

--- backend/app.py
+++ backend/app.py
@@ -9,12 +9,28 @@ api = Api(app)
 user_ns = api.namespace('user',description = '사용자 계정 API')
 image_ns = api.namespace('satellite',description = '위성 영상 데이터 API')
 #imageNum, userid, keyword, shootingtime, shootingperiod, title, color, font, url
-user_parser = reqparse.RequestParser()
+join_parser = reqparse.RequestParser()
+login_parser = reqparse.RequestParser()
 imageParser = reqparse.RequestParser()
+
+# 회원가입 
+@user_ns.route("/api/join")
+class user_join(Resource):
+    join_parser.add_argument('id', type=str, help='사용자 아이디')
+    join_parser.add_argument('pw', type=str, help='사용자 비밀번호')
+    join_parser.add_argument('name', type=str, help='사용자 이름')
+
+    @user_ns.expect(join_parser)
+    def post(self):
+        return "login"
 
 # 로그인 
 @user_ns.route("/api/login")
 class user_login(Resource):
+    login_parser.add_argument('id', type=str, help='사용자 아이디')
+    login_parser.add_argument('pw', type=str, help='사용자 비밀번호')
+
+    @user_ns.expect(login_parser)
     def post(self):
         return "login"
         
