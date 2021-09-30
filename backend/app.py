43c32f43a11175b53de2a52a49f1452eb2d0258b
sonyi00 <ksl2950@naver.com>
2021-09-30 19:12:23 +0900

    update join api

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from os import path
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restx import Api, Resource, reqparse
-
+import pymysql
 from db import create_db
 
 app = Flask(__name__)
@@ -22,7 +22,39 @@ class user_join(Resource):
 
     @user_ns.expect(join_parser)
     def post(self):
-        return "login"
+        args = join_parser.parse_args()
+        input_id = args['id']
+        input_pw = args['pw']
+        input_name = args['name']
+        
+        db = conn_db()
+        cursor= db.cursor(pymysql.cursors.DictCursor)
+        sql= "SELECT userId FROM user WHERE userId = %s;"
+        cursor.execute(sql,input_id)
+        data = cursor.fetchall()
+
+        for row in data:
+            data = row['userId']
+
+        if data:
+            db.close()
+            return jsonify({
+                "status": 409,
+                "success":False,
+                "message": "아이디 중복"
+            })
+        else:
+            sql="INSERT INTO user (userId,userPw,name) values (%s,%s,%s)"
+            cursor.execute(sql,(input_id,input_pw,input_name))
+            db.commit()
+            db.close()
+            return jsonify({
+                "status": 201,
+                "success":True,
+                "message": "회원가입 성공"
+            })
+            
+
 
 # 로그인 
 @user_ns.route("/api/login")
@@ -39,6 +71,15 @@ class user_login(Resource):
 class create_image(Resource):
     def post(self):
         return "Hello world!"
+
+def conn_db():
+    db = pymysql.connect(host='localhost',
+                        port=3306,
+                        user='root',
+                        passwd='mysql 비밀번호',
+                        db='satellite',
+                        charset='utf8')
+    return db
 
 
 if __name__ == '__main__':
