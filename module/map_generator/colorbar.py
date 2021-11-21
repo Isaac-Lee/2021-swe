de5eb28268cd537aefad0cdab62e460c39b09164
isaac_lee <yy0221ss@gmail.com>
2021-11-21 12:01:59 +0900

    delete useless files

--- module/map_generator/colorbar.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-
-def get_ticks_labels(data=None, tick=None, **text_kwargs):
-    ticks = [i for i in range(int(np.min(data)), int(np.max(data)), tick)]
-    labels = list(map(str, ticks))
-    return ticks, labels
