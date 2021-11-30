cdd43605789f1668a9b6e1c1587cca539333a59e
leecr1215 <leecr1215@naver.com>
2021-12-01 02:15:50 +0900

    feat: add shooting period and time

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,22 +1,53 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router";
 import Header from "../components/Header";
+import DatePicker from "react-datepicker";
 import "./MainPage.css";
+import "react-datepicker/dist/react-datepicker.css";
+import { ko } from "date-fns/esm/locale";
 
 const MainPage = () => {
   const history = useHistory();
   const [searchInfo, setSearchInfo] = useState({
     keyword: "전층 오존",
-    shooting_period: "",
-    shooting_time: "",
+    shooting_period: new Date(),
+    shooting_time_start: "",
+    shooting_time_end: "",
     title: "",
     font: "",
     latitude_font: "",
     longitude_font: "",
   });
 
-  const keywordList = ["전층 오존", "자외선 지수", "에어로졸"];
+  const keywordList = ["대기보정", "에어로졸 광학 두께", "엽록소농도"];
   const fontWeightList = ["20px", "15px", "10px"];
+  const timeList = [
+    "00:00",
+    "01:00",
+    "02:00",
+    "03:00",
+    "04:00",
+    "05:00",
+    "06:00",
+    "07:00",
+    "08:00",
+    "09:00",
+    "10:00",
+    "11:00",
+    "12:00",
+    "13:00",
+    "14:00",
+    "15:00",
+    "16:00",
+    "17:00",
+    "18:00",
+    "19:00",
+    "20:00",
+    "21:00",
+    "22:00",
+    "23:00",
+    "24:00",
+  ];
   const onInputChange = async (e) => {
     const { name, value } = e.target;
     setSearchInfo({
@@ -41,77 +72,124 @@ const MainPage = () => {
       </div>
       <p id="title">위성영상 전시</p>
       <div className="condition">
-        <div className="keyword">
-          <p>키워드 검색</p>
-          <select
-            name="keyword"
-            onChange={onInputChange}
-            value={searchInfo.keyword}
-          >
-            {keywordList.map((item) => (
-              <option value={item} key={item}>
-                {item}
-              </option>
-            ))}
-          </select>
+        <div className="first-line">
+          <div className="keyword">
+            <p>키워드 검색</p>
+            <select
+              name="keyword"
+              onChange={onInputChange}
+              value={searchInfo.keyword}
+            >
+              {keywordList.map((item) => (
+                <option value={item} key={item}>
+                  {item}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="title">
+            <p>제목 입력</p>
+            <input
+              type="text"
+              placeholder="  제목"
+              name="title"
+              value={searchInfo.title}
+              onChange={onInputChange}
+            ></input>
+          </div>
         </div>
-        <div className="title">
-          <p>제목 입력</p>
-          <input
-            type="text"
-            placeholder="  제목"
-            name="title"
-            value={searchInfo.title}
-            onChange={onInputChange}
-          ></input>
+        <div className="second-line">
+          <div className="shooting_period">
+            <p>촬영 날짜</p>
+            <DatePicker
+              locale={ko}
+              dateFormat="yyyy/MM/dd"
+              selected={searchInfo.shooting_period}
+              onChange={onInputChange}
+            ></DatePicker>
+          </div>
+          <div className="shooting_time">
+            <p>촬영 시간</p>
+            <select
+              name="start_time"
+              onChange={onInputChange}
+              value={searchInfo.shooting_time_start}
+            >
+              {timeList.map((item) => (
+                <option value={item} key={item}>
+                  {item}
+                </option>
+              ))}
+            </select>
+            <span>~</span>
+            <select
+              name="end_time"
+              onChange={onInputChange}
+              value={searchInfo.shooting_time_end}
+            >
+              {timeList.map((item) => (
+                <option value={item} key={item}>
+                  {item}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
-        <p id="font">폰트 크기</p>
-        <div className="font">
-          <div className="font-title">
-            <p>제목</p>
-            <select
-              name="font"
-              onChange={onInputChange}
-              value={searchInfo.font}
-            >
-              {fontWeightList.map((item) => (
-                <option value={item} key={item}>
-                  {item}
-                </option>
-              ))}
-            </select>
+        <div className="third-line">
+          <p id="font">폰트 크기</p>
+          <div className="font">
+            <div className="font-title">
+              <p>제목</p>
+              <select
+                name="font"
+                onChange={onInputChange}
+                value={searchInfo.font}
+              >
+                {fontWeightList.map((item) => (
+                  <option value={item} key={item}>
+                    {item}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="font-latitude">
+              <p>위도</p>
+              <select
+                name="latitude_font"
+                onChange={onInputChange}
+                value={searchInfo.latitude_font}
+              >
+                {fontWeightList.map((item) => (
+                  <option value={item} key={item}>
+                    {item}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="font-longitude">
+              <p>경도</p>
+              <select
+                name="longitude_font"
+                onChange={onInputChange}
+                value={searchInfo.longitude_font}
+              >
+                {fontWeightList.map((item) => (
+                  <option value={item} key={item}>
+                    {item}
+                  </option>
+                ))}
+              </select>
+            </div>
           </div>
-          <div className="font-latitude">
-            <p>위도</p>
-            <select
-              name="latitude_font"
-              onChange={onInputChange}
-              value={searchInfo.latitude_font}
-            >
-              {fontWeightList.map((item) => (
-                <option value={item} key={item}>
-                  {item}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="font-longitude">
-            <p>경도</p>
-            <select
-              name="longitude_font"
-              onChange={onInputChange}
-              value={searchInfo.longitude_font}
-            >
-              {fontWeightList.map((item) => (
-                <option value={item} key={item}>
-                  {item}
-                </option>
-              ))}
-            </select>
-          </div>
+        </div>
+        <div className="search_btn">
+          <button>검색</button>
         </div>
       </div>
-      <div className="resultImage"></div>
+      <div className="result_image"></div>
+      <div className="save_btn">
+        <button>저장</button>
+      </div>
     </div>
   );
 };
