a2fe00ec1d9e326ceda414ed0d56b8ffe4a1174b
isaac_lee <yy0221ss@gmail.com>
2021-12-07 21:33:18 +0900

    fix: module: map_generator

--- backend/map_generator/main.py
+++ backend/map_generator/main.py
@@ -8,34 +8,44 @@ from matplotlib import pyplot as plt
 if __name__ == '__main__':
     config_file_path = "./map_generator/config/config.yaml"
 
-    print("read config")
-    config_loader = ConfigLoader(file_type='yaml', file_path=config_file_path)
-    config = config_loader.get_config()
-
     print("get custom options")
     keyword = sys.argv[1]
     shooting_period = sys.argv[2]
     shooting_time = sys.argv[3]
     title = sys.argv[4]
-    font = sys.argv[5]
-    latitude_font = sys.argv[6]
-    longitude_font = sys.argv[7]
+    font = int(sys.argv[5])
+    latitude_font = int(sys.argv[6])
+    longitude_font = int(sys.argv[7])
+
+    print("read config")
+    config_loader = ConfigLoader(file_type='yaml', file_path=config_file_path)
+    config = config_loader.get_config()
+
+    title_name = config["keywords"][keyword]["keyword_name"]
+
 
     print("read data")
-    # data_file_name = "GK2B_GOCI2_L2_"+shooting_period+"_"+shooting_time+"_"+"_LA_S007_"+keywords
+
+    # data_file_name = "GK2B_GOCI2_L2_"+shooting_period+"_"+shooting_time+"_LA_S007_"+keyword
+    result_file_name = f'{keyword}_{shooting_period}_{shooting_time}_{title}_{font}'
+
+    # debug용
     data_file_name = "GK2B_GOCI2_L2_20211110_011530_LA_S007_AC"
     Data = netCDF4.Dataset(f'./map_generator/data/{data_file_name}.nc')
+    data_x_axis = []
+    data_y_axis = []
+
 
     print("make image")
-    plt.figure(figsize=(7.5, 6.8))
-    plt.plot([1, 2, 3], [1, 2, 3])
-    plt.title(title, fontname=font)
-    plt.xlabel("Longitude (deg.)", fontname=longitude_font)
-    plt.ylabel("Latitude (deg.)", fontname=latitude_font)
+    fig, ax = plt.subplots()
+    fig.set_figheight(6.8)
+    fig.set_figwidth(7.5)
+    ax.set_title(title, fontsize=font, pad=10)
+    plt.xticks(fontsize=longitude_font)
+    plt.yticks(fontsize=longitude_font)
 
-    # TODO
-    # 추후에 모든 커스텀 값들 들어오면 해당 문자열로 만들기
-    result_file_name = f'{keyword}_{shooting_period}_{shooting_time}_{title}_{font}'
+    ax.xaxis.set_label_position("top")
+    ax.set_xlabel("%s" % title_name)
 
     print("Save image...")
     plt.savefig("./map_generator/img/%s.jpg" % result_file_name, dpi=150)
