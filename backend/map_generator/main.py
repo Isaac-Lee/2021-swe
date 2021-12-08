7875ef00ce699e3253a491d17432e0357361d2e9
isaac_lee <yy0221ss@gmail.com>
2021-12-08 10:49:20 +0900

    add: module: display netCDF data

--- backend/map_generator/main.py
+++ backend/map_generator/main.py
@@ -3,13 +3,14 @@ from loader.config_loader.config_loader import ConfigLoader
 import yaml
 import sys
 import netCDF4
+import numpy as np
 from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
     config_file_path = "./map_generator/config/config.yaml"
 
     print("get custom options")
-    keyword = sys.argv[1]
+    keyword_name = sys.argv[1]
     shooting_period = sys.argv[2]
     shooting_time = sys.argv[3]
     title = sys.argv[4]
@@ -21,20 +22,33 @@ if __name__ == '__main__':
     config_loader = ConfigLoader(file_type='yaml', file_path=config_file_path)
     config = config_loader.get_config()
 
-    title_name = config["keywords"][keyword]["keyword_name"]
-
+    keyword = config["keywords"][keyword_name]
+    title_name = keyword["keyword_name"]
+    group_names = keyword["group_names"]
+    group_depth = keyword["group_depth"]
+    variable_name = keyword["variable_name"]
+    valid_min = keyword["valid_min"]
+    valid_max = keyword["valid_max"]
+    units = keyword["units"]
 
     print("read data")
 
-    # data_file_name = "GK2B_GOCI2_L2_"+shooting_period+"_"+shooting_time+"_LA_S007_"+keyword
-    result_file_name = f'{keyword}_{shooting_period}_{shooting_time}_{title}_{font}'
+    data_file_name = "GK2B_GOCI2_L2_"+shooting_period+"_"+shooting_time+"_LA_S007_"+keyword_name
+    result_file_name = f'{keyword_name}_{shooting_period}_{shooting_time}_{title}_{font}'
 
-    # debug용
-    data_file_name = "GK2B_GOCI2_L2_20211110_011530_LA_S007_AC"
+    # debug 용
+    # data_file_name = "GK2B_GOCI2_L2_20211110_011530_LA_S007_AC"
+
     Data = netCDF4.Dataset(f'./map_generator/data/{data_file_name}.nc')
-    data_x_axis = []
-    data_y_axis = []
 
+    group = Data.groups[group_names[0]]
+    for i in range(1, group_depth):
+        group = group.groups[group_names[i]]
+    var = group.variables[variable_name]
+
+    data = np.array(var)
+    data[data < valid_min] = np.nan
+    data[data > valid_max] = np.nan
 
     print("make image")
     fig, ax = plt.subplots()
@@ -43,9 +57,10 @@ if __name__ == '__main__':
     ax.set_title(title, fontsize=font, pad=10)
     plt.xticks(fontsize=longitude_font)
     plt.yticks(fontsize=longitude_font)
+    plt.imshow(data)
 
     ax.xaxis.set_label_position("top")
-    ax.set_xlabel("%s" % title_name)
+    ax.set_xlabel(f"{title_name} unit: {units}")
 
     print("Save image...")
     plt.savefig("./map_generator/img/%s.jpg" % result_file_name, dpi=150)
